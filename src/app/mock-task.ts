d244a9135f9056dac9ace1af064de22f8ee6e532
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-02-18 07:48:43 -0800

    mock-task generated

---
+++ src/app/mock-task.ts
@@ -0,0 +1,26 @@
+export const TASK=[
+  {
+    id:1,
+    text:'Doctor Appointment',
+    day:'May 15th at 2:45pm',
+    remainder:true
+  },
+  {
+    id:2,
+    text:'Sko sprint planing',
+    day:'May 16th at 6:45pm',
+    remainder:false
+  },
+  {
+    id:3,
+    text:'Went to Shop',
+    day:'May 15th at 8:45pm',
+    remainder:true
+  },
+  {
+    id:4,
+    text:'Food Shiping',
+    day:'May 15th at 10:00am',
+    remainder:false
+  }
+];
