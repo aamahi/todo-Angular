85077361761e0deadb6c48aa23d7926036c00bd0
Abdullah Al Mahi <amahi.wp@gmail.com>
2022-02-20 09:46:19 -0800

    fatch task form json-server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,16 +2,18 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from "rxjs";
 import { Task } from '../Task';
 import { TASKS } from '../mock-task';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = "http://localhost:5000/tasks"
+
+  constructor(private http:HttpClient) { }
 
   getTask(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
